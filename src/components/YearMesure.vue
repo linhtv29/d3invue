511c52a81af48025ae44847550c046eaa526bffc
Trịnh Viết Linh <linhtv@mor.com.vn>
2023-09-22 17:16:53 +0700

    start over the hill

---
+++ src/components/YearMesure.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="max-w-[600px] mx-auto py-5">
+		<div class="bg-[#aca899] font-bold">
+			<div class="text-[27px] text-[#555]">
+				<span>Are</span>
+				<span class="text-[#00aeef]">you</span>
+				<span>over the hill</span>
+			</div>
+			<div class="text-[15px] text-[#898989]">
+				<span>See how many Vietnamese are</span>
+				<span class="text-[#c0c0c0]">younger</span>
+				<span>and</span>
+				<span class="text-[#333]">older</span>
+				<span class="text-[#00aeef]">you</span>
+			</div>
+		</div>
+	</div>
+</template>
+<script setup>
+</script>

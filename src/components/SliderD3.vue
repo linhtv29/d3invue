e0cacd391ef1ce377b3b93665488f56f04768629
Trịnh Viết Linh <linhtv@mor.com.vn>
2023-09-25 11:50:18 +0700

    add slider

---
+++ src/components/SliderD3.vue
@@ -0,0 +1,65 @@
+<template>
+	<div class="relative" ref="resizeRef" id="slider-container">
+		<svg ref="svgRef"></svg>
+	</div>
+</template>
+<script setup>
+import { ref, onMounted, watchEffect, toRef, reactive } from "vue";
+import * as d3 from "d3"
+import useResizeObserver from "../use/resizeObserver.js";
+
+const emit = defineEmits(["update-val"])
+const props = defineProps({
+	value: Number,
+})
+
+const svgRef = ref(null);
+const currentValue = ref(30);
+const { resizeRef, resizeState } = useResizeObserver();
+
+onMounted(() => {
+	const svg = d3.select(svgRef.value)
+	const g = svg.append('g')
+
+	const drag = d3.drag();
+
+	watchEffect(() => {
+		g.selectAll('*').remove()
+		const { width, height } = resizeState.dimensions
+		svg.attr("width", width).attr("height", height)
+
+		g.attr('transform', `translate(0, ${height / 2})`)
+
+		const scale = d3.scaleLinear().domain([1, 99]).range([0, width])
+
+		g.append('line').attr('x1', 0).attr('y1', 0).attr('x2', width).attr('y2', 0).attr('stroke', '#f0f0f0').style("stroke-width", 5)
+		g.append('circle')
+			.attr('cx', scale(props.value || currentValue.value))
+			.attr('cy', 0)
+			.attr('r', 5)
+			.attr('fill', 'white')
+			.attr('stroke', '#666')
+			.style("stroke-width", 1)
+			.style("cursor", "pointer")
+			.call(
+				drag.on("start", dragStarted)
+						.on("drag", dragged)
+						.on("end", dragEnded)
+			)
+
+		function dragStarted(event, d) {
+			d3.select(this).raise().attr("stroke-width", 3);
+		}
+		function dragged(event, d) {
+			if(event.x < 0 || event.x > width) return
+			const value = scale.invert(event.x).toFixed(0)
+			currentValue.value = value
+			emit("update-val", value)
+		}
+
+		function dragEnded(event, d) {
+			d3.select(this).attr("stroke-width", 1);
+		}
+	})
+})
+</script>
--- src/views/YearMesure.vue
+++ src/views/YearMesure.vue
@@ -1,20 +1,34 @@
 <template>
 	<div class="max-w-[600px] mx-auto py-5">
-		<div class="bg-[#aca899] font-bold">
+		<div class="bg-[#f0f0f0] font-bold p-2">
 			<div class="text-[27px] text-[#555]">
-				<span>Are</span>
+				<span> Are </span>
 				<span class="text-[#00aeef]">you</span>
-				<span>over the hill</span>
+				<span> over the hill</span>
 			</div>
 			<div class="text-[15px] text-[#898989]">
-				<span>See how many Vietnamese are</span>
+				<span>See how many Vietnamese are </span>
 				<span class="text-[#c0c0c0]">younger</span>
-				<span>and</span>
-				<span class="text-[#333]">older</span>
-				<span class="text-[#00aeef]">you</span>
+				<span> and </span>
+				<span class="text-[#333]"> older </span>
+				<span class="text-[#00aeef]"> you </span>
 			</div>
+		</div>
+		<div class="mt-2 text-[#00aeef] flex">
+			<div class="text-[15px] w-[72px] text-ellipsis text-left overflow-hidden whitespace-pre cursor-default"
+				title="Move slider to select your age">Move slider to select your age</div>
+			<SliderD3 class="max-h-[22px] flex-1" @update-val="age = $event" :value="age" />
+			<div class="w-[152px] h-10 flex justify-center items-center text-4xl font-bold">{{ age }}</div>
+		</div>
+
+		<div class="text-[#00aeef] border-t border-[#aeaeae] mt-2 h-16 flex justify-center items-center border-b bg-[#f1fdff] font-bold text-xl">
+			{{ `You are older than ${age}% of All Vietnamese` }}
 		</div>
 	</div>
 </template>
 <script setup>
+import { ref } from "vue";
+import SliderD3 from '../components/SliderD3.vue';
+
+const age = ref(31);
 </script>
